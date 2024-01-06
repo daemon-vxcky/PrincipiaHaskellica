6388cb269c6722c533180defa5490909b6a66a5c
Vignesh160803 <vig160803@gmail.com>
2024-01-06 23:37:13 +0530

    made changes to pre existing codes

---
+++ conditionals/greatest.hs
@@ -0,0 +1,10 @@
+main :: IO()
+
+main = do
+    let n1 = 254
+    let n2 = -74
+
+    if n1>n2
+        then putStrLn $ show n1 ++ " is greater than " ++ show n2 ++ "."
+    else 
+        putStrLn $ show n2 ++ " is greater than " ++ show n1 ++ "."
