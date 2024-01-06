15793322cac9514ed6f9406496cdfd45b4fd05c6
Vignesh160803 <vig160803@gmail.com>
2024-01-06 23:44:26 +0530

    made changes to pre existing codes

--- conditionals/greatest.hs
+++ conditionals/greatest.hs
@@ -6,5 +6,7 @@ main = do
 
     if n1>n2
         then putStrLn $ show n1 ++ " is greater than " ++ show n2 ++ "."
-    else 
-        putStrLn $ show n2 ++ " is greater than " ++ show n1 ++ "."
+    else if n1<n2
+        then putStrLn $ show n2 ++ " is greater than " ++ show n1 ++ "."
+    else
+        putStrLn $ show n1 ++ " and " ++ show n2 ++ " are equal."
