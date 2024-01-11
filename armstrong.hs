1f29f3d83d5c31c21e6513ebb5dae524d4b9a67b
Vignesh160803 <vig160803@gmail.com>
2024-01-11 17:30:15 +0530

    added a file for haskell coding

---
+++ armstrong.hs
@@ -0,0 +1,22 @@
+isArmstrong :: Int -> Bool
+isArmstrong n = n == sumOfPowers n (numDigits n)
+
+sumOfPowers :: Int -> Int -> Int
+sumOfPowers n 0 = 0
+sumOfPowers n k = (digit ^ k) + sumOfPowers remainingDigits k
+    where
+        digit = n `mod` 10
+        remainingDigits = n `div` 10
+
+numDigits :: Int -> Int
+numDigits n
+    | n < 10 = 1
+    | otherwise = 1 + numDigits (n `div` 10)
+
+main :: IO()
+main = do
+    let n = 153
+    if isArmstrong n
+        then putStrLn $ "The number is an Armstrong number"
+    else
+        putStrLn $ "The number is not an Armstrong number"
