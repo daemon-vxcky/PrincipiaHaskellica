00563085adc2438091622c38f5ea1ffaba5149ef
Vignesh160803 <vig160803@gmail.com>
2024-01-29 13:57:48 +0530

    added a file for haskell coding

---
+++ assignment-1/address.hs
@@ -0,0 +1,7 @@
+main = do
+    let aline1 = "706 B, Avinashi Road"
+    let aline2 = "Pappanaickenpalayam"
+    let city = "Coimbatore"
+    let state = "Tamil Nadu"
+    putStrLn $ "My address is : " ++ aline1 ++ ", " ++ aline2 ++ ", " ++ city ++ ", " ++ state
+    
